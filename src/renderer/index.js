e4b70c354cd8360b866daf1cb78ec4df53f53d4e
Ross Rosen <rrosen326@gmail.com>
2018-12-06 06:00:38 -0800

    prettified template string

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -18,6 +18,18 @@ function init() {
         require('electron').shell.openExternal(b)
       }
     },
-    template: `<div><div class=empty><p class="empty-title h5">Welcome to your new project!<p class=empty-subtitle>Get qwdqwd now and take advantage of the great documentation at hand.<div class=empty-action><button @click="open('https://webpack.electron.build')"class="btn btn-primary">Documentation</button> <button @click="open('https://electron.atom.io/docs/')"class="btn btn-primary">Electron</button><br><ul class=breadcrumb><li class=breadcrumb-item>electron-webpack v{{ versions.electronWebpack }}</li><li class=breadcrumb-item>electron v{{ versions.electron }}</li></ul></div><p class=footer>This intitial landing page can be easily removed from <code>src/renderer/index.js</code>.</p></div></div>`
+    template: `
+<div>
+  <div class=empty>
+    <p class="empty-title h5">Welcome to your new project!<p class=empty-subtitle>Get qwdqwd now and take advantage of the great documentation at hand.<div class=empty-action><button @click="open('https://webpack.electron.build')" class="btn btn-primary">Documentation</button> <button @click="open('https://electron.atom.io/docs/')" class="btn btn-primary">Electron</button><br>
+          <ul class=breadcrumb>
+            <li class=breadcrumb-item>electron-webpack v{{ versions.electronWebpack }}</li>
+            <li class=breadcrumb-item>electron v{{ versions.electron }}</li>
+          </ul>
+        </div>
+        <p class=footer>This intitial landing page can be easily removed from <code>src/renderer/index.js</code>.</p>
+  </div>
+</div>
+`
   }).$mount('#app')
 }
