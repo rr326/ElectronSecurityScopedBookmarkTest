0e2754d958f28a651f4358c74469dce37d18b930
Ross Rosen <rrosen326@gmail.com>
2018-12-06 06:29:44 -0800

    working cleaned boilerplate

---
+++ src/renderer/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>App.vue</h1>
+    <p>lorem ipsum </p>
+  </div>
+</template>
+
+<script>
+export default {
+
+  name: 'app',
+
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
