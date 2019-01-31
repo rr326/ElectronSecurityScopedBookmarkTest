50c184c8986c1d90d1cb08190c24e2e654aedb71
Ross Rosen <rrosen326@gmail.com>
2019-01-31 14:21:00 -0800

    update

--- src/shared/dialog.js
+++ src/shared/dialog.js
@@ -1,33 +1,6 @@
 /**
  * Shared code to ensure that renderer and main processes are using the exact same code.
  */
-
-// Main
-//   dialog.showOpenDialog({
-//           title: 'MAIN WINDOW',
-//           message: "MAIN WINDOW",
-//           defaultPath: '/Users/rrosen',
-//           buttonLabel: 'MAIN Select Directory',
-//           properties: ["openDirectory", "createDirectory"],
-//           securityScopedBookmarks: true,
-//         }, (filePaths, bookmarks) => {
-//           dialog.showMessageBox({
-//             type: "info",
-//             buttons: ['ok'],
-//             title: "Open Dialog Results",
-//             message: `\
-// ShowOpenDialog Results: \n\
-// isMas: ${process.mas}\n\
-// filePaths: |${filePaths}|\n\
-// bookmarks: |${bookmarks}|\n\
-// typeof bookmarks: ${typeof bookmarks} \n\
-// isArray: ${bookmarks instanceof Array}\n\
-// length: ${bookmarks instanceof Array ? bookmarks.length : 'NA'}\n\
-// bookmarks ==== [""]: ${bookmarks === [""]}\
-// `
-//           })
-//         })
-//
 function resultsText(filePaths, bookmarks, securityScopedBookmarksSetting) {
   return `\
 ShowOpenDialog Results: \n\
